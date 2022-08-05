1dc78111c74722d5af754d46742652df727cc4e3
Jeeann <jeeann.kim@gmail.com>
2022-08-05 23:52:11 +0100

    move from other folder

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id: 1,
+        task: 'Eat',
+        complete: true
+    },
+    {
+        id: 2,
+        task: 'Sleep',
+        complete: true
+    },
+    {
+        id: 3,
+        task: 'Coding',
+        complete: false
+    },
+];
+
+export default data;
